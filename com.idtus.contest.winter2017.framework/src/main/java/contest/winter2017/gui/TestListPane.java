c57dfa7bfc6a66c0d8dd76d9cf68d02ade1190b0
J3698 <2018asanders@tjhsst.edu>
2017-01-29 19:22:29 -0500

    Implement drag and drop

--- com.idtus.contest.winter2017.framework/src/main/java/contest/winter2017/gui/TestListPane.java
+++ com.idtus.contest.winter2017.framework/src/main/java/contest/winter2017/gui/TestListPane.java
@@ -9,8 +9,11 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -41,8 +44,7 @@ class TestListPane extends ScrollPane {
 		setFitToWidth(true);
 		setHbarPolicy(ScrollBarPolicy.NEVER);
 		setVbarPolicy(ScrollBarPolicy.ALWAYS);
-		// setStyle("-fx-focus-color: transparent; -fx-faint-focus-color:
-		// transparent;");
+		setStyle("-fx-focus-color: transparent; -fx-faint-focus-color: transparent;");
 
 		this.contentPane = new VBox(SPACING);
 		setContent(this.contentPane);
@@ -52,13 +54,23 @@ class TestListPane extends ScrollPane {
 	}
 
 	/**
+	 * Selects the given testInfo.
+	 * 
+	 * @param testInfo
+	 *            - TestInfo to select
+	 */
+	public void selectTest(TestInfo testInfo) {
+
+	}
+
+	/**
 	 * Adds a test.
 	 * 
 	 * @param test
 	 *            - test to add
 	 */
-	public void addTest(TestInfo testInfo) {
-		this.contentPane.getChildren().add(infoAdderIndex, testInfo);
+	public void addTest(File testInfo) {
+		this.contentPane.getChildren().add(infoAdderIndex, new TestInfo(this));
 		this.infoAdderIndex++;
 	}
 }
@@ -72,7 +84,7 @@ class TestInfo extends VBox {
 	/**
 	 * Constructs a TestInfo.
 	 */
-	public TestInfo() {
+	public TestInfo(TestListPane pane) {
 		setAlignment(Pos.CENTER);
 		setStyle("-fx-border-color: lightgray; -fx-border-width: 1; -fx-focus-color: black;");
 		setFocused(true);
@@ -95,7 +107,7 @@ class TestInfo extends VBox {
 			 */
 			public void handle(MouseEvent event) {
 				if (event.getButton() == MouseButton.PRIMARY) {
-					System.out.println("Hello");
+					pane.selectTest(TestInfo.this);
 				}
 			}
 		});
@@ -108,6 +120,7 @@ class TestInfo extends VBox {
  * @author ICT-2
  */
 class TestInfoAdder extends VBox {
+
 	/**
 	 * Constructs a new TestInfoAdder.
 	 */
@@ -119,6 +132,50 @@ class TestInfoAdder extends VBox {
 		Text drag = new Text("Or Drag and Drop Jar");
 		drag.setFont(new Font(10));
 		getChildren().addAll(new VExternSpace(addButton, 13, 4), new VExternSpace(drag, 4, 13));
+
+		setOnDragEntered(new EventHandler<DragEvent>() {
+			public void handle(DragEvent event) {
+				Dragboard db = event.getDragboard();
+				if (db.hasFiles() && db.getFiles().size() == 1) {
+					if (db.getFiles().get(0).getPath().endsWith(".jar")) {
+						setStyle("-fx-border-color: #039ED3; -fx-border-width: 2;");
+					}
+				}
+
+				event.consume();
+			}
+		});
+		setOnDragOver(new EventHandler<DragEvent>() {
+			public void handle(DragEvent event) {
+				Dragboard db = event.getDragboard();
+				if (db.hasFiles() && db.getFiles().size() == 1) {
+					if (db.getFiles().get(0).getPath().endsWith(".jar")) {
+						event.acceptTransferModes(TransferMode.COPY);
+					}
+				}
+
+				event.consume();
+			}
+		});
+		setOnDragExited(new EventHandler<DragEvent>() {
+			public void handle(DragEvent event) {
+				if (event.isAccepted()) {
+					setStyle("-fx-border-color: lightgray; -fx-border-width: 1;");
+				}
+				event.consume();
+			}
+		});
+		setOnDragDropped(new EventHandler<DragEvent>() {
+			public void handle(DragEvent event) {
+				Dragboard db = event.getDragboard();
+				System.out.println(db.hashCode());
+				if (db.hasFiles()) {
+					testList.addTest(db.getFiles().get(0));
+				}
+				setStyle("-fx-border-color: lightgray; -fx-border-width: 1;");
+				event.consume();
+			}
+		});
 	}
 }
 
@@ -152,7 +209,7 @@ class SelectJarButton extends Button {
 				Window window = SelectJarButton.this.getScene().getWindow();
 				File selected = fc.showOpenDialog(window);
 				if (selected != null && selected.exists()) {
-					testLists.addTest(new TestInfo());
+					testLists.addTest(selected);
 				}
 			}
 		});
