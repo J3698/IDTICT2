2c3f32fcd1a18a675bea696082ff58eb103ae056
J3698 <2018asanders@tjhsst.edu>
2017-02-10 22:36:45 -0500

    Add memory monitor to main

--- com.idtus.contest.winter2017.framework/src/main/java/contest/winter2017/gui/GUIMain.java
+++ com.idtus.contest.winter2017.framework/src/main/java/contest/winter2017/gui/GUIMain.java
@@ -1,8 +1,10 @@
 package contest.winter2017.gui;
 
+import java.math.BigDecimal;
 import java.util.Random;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -16,6 +18,7 @@ import javafx.stage.WindowEvent;
  */
 public class GUIMain extends Application {
 	private static String guiID = null;
+	private static boolean isRunning = true;
 
 	/**
 	 * Starts the GUI.
@@ -33,18 +36,34 @@ public class GUIMain extends Application {
 			@Override
 			public void handle(WindowEvent event) {
 				testLists.stopTests();
+				isRunning = false;
 			}
 		});
 
 		stage.show();
 
-		/*
-		 * BorderPane pane = new BorderPane(); pane.setLeft(new
-		 * TestListPane(pane)); TestListPane tPane = new TestListPane(pane);
-		 * pane.setCenter(new MainPane(new GUITestPackage(tPane, "", new
-		 * File(".")))); Scene scene = new Scene(pane, 640, 480);
-		 * stage.setScene(scene); stage.show();
-		 */
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (GUIMain.this.isRunning) {
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException ie) {
+					}
+					Runtime runtime = Runtime.getRuntime();
+					BigDecimal free = new BigDecimal(runtime.freeMemory() + ".00");
+					BigDecimal total = new BigDecimal(runtime.totalMemory() + ".00");
+					BigDecimal percent = free.divide(total, BigDecimal.ROUND_HALF_UP);
+					Platform.runLater(new Runnable() {
+						@Override
+						public void run() {
+							System.out.println(percent);
+						}
+					});
+				}
+			}
+
+		}).start();
 	}
 
 	public static String getGuiID() {
